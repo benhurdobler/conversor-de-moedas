cee98ab218f67fefcb017ed6dc8eb463f5ba95b1
benhurdobler <benhurdobler1003@gmail.com>
2024-03-18 11:49:38 -0300

    segundo-commit

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const convertButton = document.querySelector(".conversor")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues(){
+    const currencyInputValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+
+    console.log(currencySelect.value)
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+    
+
+    if(currencySelect.value == "dolar"){
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }) .format(currencyInputValue / dolarToday)
+
+    }
+    if(currencySelect.value == "euro"){
+        
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR" 
+        }) .format(currencyInputValue / euroToday)
+
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }) .format(currencyInputValue)
+
+}
+
+function changeCurrency(){
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if(currencySelect.value == "dolar"){
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assets/dolar.png"
+    }
+    if(currencySelect.value == "euro"){
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+    convertValues()
+}
+
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
